a6b6dc17af67a4ca5c92cf877b5027b5ab7ef77e
Thomas Treffner <treffner@logmedia.at>
2021-04-06 08:09:50 +0200

    Update simple.v
    dotenv -> vdotenv

--- examples/simple.v
+++ examples/simple.v
@@ -6,7 +6,7 @@ fn main() {
 	vdotenv.load()
 	// you can use build in os.getenv()
 	println(os.getenv('POSTGRES_HOST'))
-	// you can also use dotenv.get() if you need fallback handling
+	// you can also use vdotenv.get() if you need fallback handling
 	secret := vdotenv.get('JWT_SECRET') or {
 		'default_dev_token' // default, not found, or simply the same on all environments
 	}
